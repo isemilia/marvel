a4f36dc1d493e8ee131d622301b802b5f832bb6c
Emilia <81705094+emiliaweb@users.noreply.github.com>
2023-01-13 13:21:03 +0600

    Validate props

--- src/components/charInfo/CharInfo.js
+++ src/components/charInfo/CharInfo.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import MarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
@@ -102,6 +103,10 @@ const View = ({char}) => {
             <ul className="char__comics-list">{comicsElems.length !== 0 ? comicsElems : 'There are no comics for this character'}</ul>
         </>
     )
+}
+
+CharInfo.propTypes = {
+    charID: PropTypes.number
 }
 
 export default CharInfo;
--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import MarvelService from '../../services/MarvelService';
 import Spinner from '../spinner/Spinner';
@@ -119,6 +120,10 @@ class CharList extends Component {
             </div>
         )
     }
+}
+
+CharList.propTypes = {
+    onCharSelected: PropTypes.func
 }
 
 export default CharList;
